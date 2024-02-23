9868c70c4ba006768aa95051480dbe2caaf35deb
Devilal panchal <devilalpanchal551@gmail.com>
2024-02-23 11:36:12 +0530

    for input to maintain height

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ secondContainer.addEventListener('mouseover', () => {
     secondbtn.classList.remove('hide')
     secondbtn.style.background = "rgb(222, 241, 243)"
     secondbtn.style.width = "100%"
-    secondbtn.style.height = "12%"
+    secondbtn.style.height = "20%"
     secondbtn.style.padding = "10px"
     secondbtn.style.textAlign = "start";
     secondbtn.style.paddingLeft = "20px";
@@ -23,7 +23,7 @@ thirdContainer.addEventListener('mouseover', () => {
     thirdbtn.style.background = "rgb(222, 241, 243)"
     thirdbtn.style.width = "100%"
     thirdbtn.style.padding = "10px"
-    thirdbtn.style.height = "12%";
+    thirdbtn.style.height = "20%";
     thirdbtn.style.textAlign = "start";
     thirdbtn.style.paddingLeft = "20px";
 })
@@ -31,6 +31,121 @@ thirdContainer.addEventListener('mouseout', () => {
     thirdbtn.classList.add('hide')
 })
 ///// My Code End
+
+
+let btn = document.querySelectorAll('.btn')
+console.log(btn)
+let input = document.querySelectorAll('.issue')
+console.log(input)
+
+btn.forEach((item) => {
+  console.log(item)
+  item.addEventListener('click', (e) => {
+    console.log(e.target)
+    let input = e.target.nextElementSibling
+    console.log(input)
+    input.classList.remove('hide')
+    e.target.classList.add('hide')
+    // let inputTag=e.target.nextElementSibling 
+    input.focus()
+   
+    // input.classList.remove('hide')
+    // btn.classList.add('hide')
+    // input.focus()
+  })
+})
+
+// btn.addEventListener('click', () => {
+//   input.classList.remove('hide')
+//   btn.classList.add('hide')
+//   input.focus()
+// })
+
+let draggingInfo = {
+  draggingElement: null,
+  draggingId: null,
+}
+
+input.forEach((item) => {
+  item.addEventListener('keyup', (e) => {
+    console.log(e.target)
+    let input = e.target
+    if (e.keyCode == 13) {
+      let value = input.value
+      let card = document.createElement('div')
+      card.innerText = value
+      card.className = 'child-card'
+      card.style.padding = '10px'
+      card.style.borderRadius = '5px'
+      card.style.marginTop = '5px'
+      card.setAttribute('data-container', 'card')
+      card.style.background = 'orange'
+      card.draggable = 'true'
+      console.log(card);
+
+ card.addEventListener('dragstart', (e) => {
+   console.log('hello')
+   draggingInfo.draggingElement = e.target
+   draggingInfo.draggingId = e.target.getAttribute('data-container')
+   console.log(draggingInfo.draggingElement, draggingInfo.draggingId)
+ })
+
+      let parentContainer = e.target.parentElement
+      parentContainer.appendChild(card)
+      e.target.classList.add('hide')
+      e.target.previousElementSibling.classList.remove('hide')
+      e.target.value = ''
+
+      // console.log(value)
+    }
+  })
+})
+
+// to add function for quickstart button
+
+let Quickbutton = document.querySelector('.Quickbutton')
+let QuickStart1= document.querySelector('.quickstart1')
+let openQuick = document.querySelector('.yourCard')
+
+
+// QuickStart1.addEventListener('click',()=>{
+//   Quickbutton.classList.add('.hide')
+// })
+
+openQuick.addEventListener('click',()=>{
+  Quickbutton.classList.remove('.hide')
+})
+
+
+let allContainer = document.querySelectorAll('.container')
+console.log(allContainer)
+
+allContainer.forEach((item) => {
+  item.addEventListener('dragover', (event) => {
+    event.preventDefault()
+  })
+
+  item.addEventListener('drop', (e) => {
+    console.log('dropping card')
+    let card = draggingInfo.draggingElement
+    card.id = draggingInfo.draggingId
+    card.style.background = 'gray'
+
+    let box = e.currentTarget
+    console.log(box)
+    box.appendChild(card)
+  })
+})
+
+
+if (card == drop) {
+
+  
+  card.style.background = 'green'
+  
+}
+
+
 
 // btn.addEventListener('click', () => {
 
@@ -94,97 +209,6 @@ thirdContainer.addEventListener('mouseout', () => {
 
 // })
 
-let btn = document.querySelectorAll('.btn')
-console.log(btn)
-let input = document.querySelectorAll('.issue')
-console.log(input)
-
-btn.forEach((item) => {
-  console.log(item)
-  item.addEventListener('click', (e) => {
-    console.log(e.target)
-    let input = e.target.nextElementSibling
-    console.log(input)
-    input.classList.remove('hide')
-    e.target.classList.add('hide')
-    // let inputTag=e.target.nextElementSibling 
-    input.focus()
-   
-    // input.classList.remove('hide')
-    // btn.classList.add('hide')
-    // input.focus()
-  })
-})
-
-// btn.addEventListener('click', () => {
-//   input.classList.remove('hide')
-//   btn.classList.add('hide')
-//   input.focus()
-// })
-
-let draggingInfo = {
-  draggingElement: null,
-  draggingId: null,
-}
-
-input.forEach((item) => {
-  item.addEventListener('keyup', (e) => {
-    console.log(e.target)
-    let input = e.target
-    if (e.keyCode == 13) {
-      let value = input.value
-      let card = document.createElement('div')
-      card.innerText = value
-      card.className = 'child-card'
-      card.style.padding = '10px'
-      card.style.borderRadius = '10px'
-      card.style.marginTop = '5px'
-      card.setAttribute('data-container', 'card')
-      card.style.background = 'orange'
-      card.draggable = 'true'
-      console.log(card);
-
- card.addEventListener('dragstart', (e) => {
-   console.log('hello')
-   draggingInfo.draggingElement = e.target
-   draggingInfo.draggingId = e.target.getAttribute('data-container')
-   console.log(draggingInfo.draggingElement, draggingInfo.draggingId)
- })
-
-      let parentContainer = e.target.parentElement
-      parentContainer.appendChild(card)
-      e.target.classList.add('hide')
-      e.target.previousElementSibling.classList.remove('hide')
-      e.target.value = ''
-
-      // console.log(value)
-    }
-  })
-})
-
-// to add function for quickstart button
-
-let Quickbutton = document.querySelector('.Quickbutton')
-let QuickStart1= document.querySelector('.quickstart1')
-let openQuick = document.querySelector('.yourCard')
-
-
-// QuickStart1.addEventListener('click',()=>{
-//   Quickbutton.classList.add('.hide')
-// })
-
-openQuick.addEventListener('click',()=>{
-  Quickbutton.classList.remove('.hide')
-})
-
-
-
-
-
-
-
-
-
 
 
 // input.addEventListener('keyup', (e) => {
@@ -217,30 +241,3 @@ openQuick.addEventListener('click',()=>{
 //   }
 // })
 
-let allContainer = document.querySelectorAll('.container')
-console.log(allContainer)
-
-allContainer.forEach((item) => {
-  item.addEventListener('dragover', (event) => {
-    event.preventDefault()
-  })
-
-  item.addEventListener('drop', (e) => {
-    console.log('dropping card')
-    let card = draggingInfo.draggingElement
-    card.id = draggingInfo.draggingId
-    card.style.background = 'gray'
-
-    let box = e.currentTarget
-    console.log(box)
-    box.appendChild(card)
-  })
-})
-
-
-if (card == drop) {
-
-  
-  card.style.background = 'green'
-  
-}
